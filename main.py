562a0f4076895b7f638bce7b237f6eeceec0ebc0
r3Vibe <arnab95gupta@gmail.com>
2022-01-15 10:32:11 +0000

    updates

--- main.py
+++ main.py
@@ -1,47 +1,50 @@
 from datetime import datetime
-import webbrowser, time, pyautogui, keyboard, os, cv2, pytesseract, json
+from glob import glob
+import webbrowser, time, pyautogui, os, cv2, pytesseract, json
 from PIL import Image, ImageGrab,ImageEnhance
 from PIL import ImageOps
 import numpy as nm
 
 # positions
-bc_connectwallet = (962,624)
-bc_wallpass = (1616,356)
-bc_sign = (1824,547)
-bc_huunt = (974,441)
-bc_hero = (960,719)
+
 #farmersworld
-fw_login = (962,916)
-fw_wax = (967,518)
-fw_stoneaxe = (626,383)
-fw_encientaxe = (625,470)
-fw_work = (1021,668)
-fw_repair = (1203,667)
-fw_map = (1069,958)
-fw_mining = (749,443)
-fw_plant = (746,667)
-fw_chicken = (1163,440)
-fw_seed1 = (627,386)
-fw_seed2 = (627,475)
-fw_seed3 = (627,561)
-fw_seed4 = (627,647)
-fw_openarea = (303,276)
-fw_addenergy = (1698,148)
-fw_energyinput = (970,531)
-fw_exchange = (963,612)
+fw_login = (682,692)
+fw_wax = (688,442)
+fw_stoneaxe = (466,351)
+fw_encientaxe = (468,408)
+fw_fishrod = (467,462)
+fw_work = (725,536)
+fw_repair = (857,534)
+fw_map = (751,718)
+fw_mining = (542,394)
+fw_plant = (541,533)
+fw_chicken = (816,380)
+fw_seed1 = (466,348)
+fw_seed2 = (466,408)
+fw_seed3 = (466,465)
+fw_seed4 = (466,524)
+fw_seed5 = (466,544)
+fw_chicken1 = (465,344)
+fw_openarea = (203,247)
+fw_addenergy = (1162,195)
+fw_energyinput = (706,439)
+fw_exchange = (686,501)
 
 #timer
 stoneaxe_click = ""
 encientaxe_click = ""
+fishrod_click = ""
 seed1_click = ""
 seed2_click = ""
 seed3_click = ""
 seed4_click = ""
+seed5_click = ""
 script_start = time.time()
 
 # clicks
 stoneaxe = 0
 encientaxe = 0
+seed5 = 0
 seed4 = 0
 seed3 = 0
 seed2 = 0
@@ -50,16 +53,20 @@ seed1 = 0
 def setclick_vals():
     global stoneaxe
     global encientaxe
+    global fishrod
+    global seed5
     global seed4
     global seed3
     global seed2
     global seed1
 
-    with open('click.json', 'r') as openfile:
+    with open('/home/kali/Desktop/farmersworldauto/click.json', 'r') as openfile:
         json_object = json.load(openfile)
 
     stoneaxe = json_object['stoneaxe']
     encientaxe = json_object['encientaxe']
+    fishrod = json_object['fishrod']
+    seed5 = json_object['seed5']
     seed4 = json_object['seed4']
     seed3 = json_object['seed3']
     seed2 = json_object['seed2']
@@ -70,21 +77,76 @@ def setclick_vals():
 
 def open_site():
     webbrowser.open_new('https://play.farmersworld.io')
-    time.sleep(10)
+    time.sleep(25)
+    #pyautogui.press('f11')
+    #time.sleep(5)
     login()
 
 
 def login():
     pyautogui.click(fw_login)
-    time.sleep(5)
+    time.sleep(10)
     pyautogui.click(fw_wax)
     time.sleep(10)
-    stoneaxe_mine()
+    fishrod_mine()
 
+
+def fishrod_mine():
+    global fishrod_click
+    global fishrod
+    global script_start
+    goto_fishrod()
+    
+    if fishrod_click == "":
+        if check_timer() == "00:00:00":
+            if int(fishrod) <= 24:
+                pyautogui.click(fw_work)
+                time.sleep(10)
+                pyautogui.click(fw_openarea)
+                time.sleep(5)
+
+                if check_timer() == "00:00:00":
+                    fishrod_mine()
+                else:
+                    fishrod = int(fishrod) + 1
+                    addclick('fishrod',fishrod)
+
+                    fishrod_click = time.time()
+                    script_start = time.time()
+                    stoneaxe_mine()
+            else:
+                stoneaxe_mine()
+        else:
+            stoneaxe_mine()
+    else:
+        if round(((time.time() - fishrod_click)/3600)) >= 1:
+            if check_timer() == "00:00:00":
+                if int(fishrod) <= 24:
+                    pyautogui.click(fw_work)
+                    time.sleep(10)
+                    pyautogui.click(fw_openarea)
+                    time.sleep(5)         
+
+                    if check_timer() == "00:00:00":
+                        fishrod_mine()
+                    else:
+                        fishrod = int(fishrod) + 1
+                        addclick('fishrod',fishrod)
+
+                        fishrod_click = time.time()
+                        script_start = time.time()
+
+                        stoneaxe_mine()
+                else:
+                    addclick('fishrod',0)
+                    stoneaxe_mine()
+            else:
+                stoneaxe_mine()
+        else:
+            stoneaxe_mine()
 
 def stoneaxe_mine():
     global stoneaxe_click
-    global script_start
     global stoneaxe
 
     goto_stoneaxe()
@@ -107,7 +169,6 @@ def stoneaxe_mine():
                     addclick('stoneaxe',stoneaxe)
 
                     stoneaxe_click = time.time()
-                    script_start = time.time()
 
                     encientaxe_mine()
             else:
@@ -133,7 +194,6 @@ def stoneaxe_mine():
                         addclick('stoneaxe',stoneaxe)
 
                         stoneaxe_click = time.time()
-                        script_start = time.time()
 
                         encientaxe_mine()
                 else:
@@ -429,12 +489,12 @@ def repeat_mine():
         
 
 def addclick(name,val):
-    with open('click.json', 'r') as openfile:
+    with open('/home/kali/Desktop/farmersworldauto/click.json', 'r') as openfile:
         json_object = json.load(openfile)
 
     json_object[name] = val
 
-    with open('click.json', 'w') as openfile:
+    with open('/home/kali/Desktop/farmersworldauto/click.json', 'w') as openfile:
         json.dump(json_object,openfile)
 
 
@@ -443,11 +503,19 @@ def add_energy(amt):
     time.sleep(5)
     pyautogui.click(fw_energyinput)
     time.sleep(5)
-    keyboard.write(str(amt))
+    pyautogui.write(str(amt))
     time.sleep(5)
     pyautogui.click(fw_exchange)
     time.sleep(10)
     pyautogui.click(fw_openarea)
+    time.sleep(5)
+
+def goto_fishrod():
+    pyautogui.click(fw_map)
+    time.sleep(5)
+    pyautogui.click(fw_mining)
+    time.sleep(5)
+    pyautogui.click(fw_fishrod)
     time.sleep(5)
 
 
@@ -502,12 +570,11 @@ def goto_seed4():
 
 
 def check_timer():
-    box = (1008,582,1214,632)
+    box = (708,463,850,513)
     image = ImageGrab.grab(bbox=box)
     text = pytesseract.image_to_string(cv2.cvtColor(nm.array(image),cv2.COLOR_BGR2GRAY),lang='eng')
     ocr = text.replace("\n\x0c", "") 
     print(ocr)
-    # print(ocr.strip() == "00:00:00")
     ocr = ocr.strip()
     return ocr
 
@@ -522,7 +589,7 @@ def close():
     finally:
         os.system(f'taskkill /F /PID {pid}')
 
-keyboard.add_hotkey('ctrl+q',close)
+#keyboard.add_hotkey('ctrl+q',close)
 
 
 def printvals():
@@ -533,8 +600,9 @@ def printvals():
 
 if __name__ == '__main__':
     # open_site()
-    # check_timer()
-    # print(pyautogui.position())
+    #check_timer()
+    print(pyautogui.position())
     # stoneaxe_mine()
     # add_energy(200)
-    setclick_vals()
+    #setclick_vals()
+    #pyautogui.moveTo(fw_login)
--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-import json
-
-
-def addclick(name,val):
-    with open('click.json', 'r') as openfile:
-        json_object = json.load(openfile)
-
-    json_object[name] = val
-
-    with open('click.json', 'w') as openfile:
-        json.dump(json_object,openfile)
-
-
-
-addclick('seed4',0)
+import pyautogui
